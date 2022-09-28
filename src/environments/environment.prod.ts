8cd3374f19916b5f2494c82bfb944c209ee590d1
jimmy <rootkaliente@gmail.com>
2022-09-28 11:46:21 -0500

    [Jimmy] agregar variable de entorno en prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,14 @@
 export const environment = {
   production: true,
   hmr: false,
-  apiUrl: 'https://api.bigpuntos.com'
+  apiUrl: 'https://api.bigpuntos.com',
+  firebaseConfig: {
+    apiKey: 'AIzaSyBISFdku_C2M-rr4-bhpkeXtE4z8u07yRo',
+    authDomain: 'grp-bigpuntos.firebaseapp.com',
+    projectId: 'grp-bigpuntos',
+    storageBucket: 'grp-bigpuntos.appspot.com',
+    messagingSenderId: '1056585428972',
+    appId: '1:1056585428972:web:b9991af9c50becb97d5ac9',
+    measurementId: 'G-N6K6WZ5T59'
+  }
 };

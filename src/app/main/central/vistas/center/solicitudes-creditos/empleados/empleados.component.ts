b18c6be24a5fcbf540abd54d0a72195b922b1928
jimmy <rootkaliente@gmail.com>
2023-09-27 12:35:04 -0500

    [Jimmy] correcion de columnas de aplicacion de locales en la solicitudes-creditos

--- src/app/main/central/vistas/center/solicitudes-creditos/empleados/empleados.component.ts
+++ src/app/main/central/vistas/center/solicitudes-creditos/empleados/empleados.component.ts
@@ -288,7 +288,7 @@ export class EmpleadosComponent implements OnInit, AfterViewInit {
     }
 
     modalSelectOpen(modalSelect, empresasAplican) {
-        this.selectEmpresasCorpSelected = empresasAplican;
+        this.selectEmpresasCorpSelected = JSON.parse(empresasAplican);
         this.modalService.open(modalSelect, {
             windowClass: 'modal'
         });
@@ -331,4 +331,6 @@ export class EmpleadosComponent implements OnInit, AfterViewInit {
         });
 
     }
+
+    protected readonly JSON = JSON;
 }
